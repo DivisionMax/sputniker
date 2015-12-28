b0a7f4f1cea1b4e8d249f5fe437fa467e24fff3e
Gavin Wiener <gavinwiener@gmail.com>
2015-12-28 18:17:27 +0200

    Jquery ajax working and images loading

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 /*Container for review data*/
-var content = document.getElementById("content"); 
+// var content = document.getElementById("content"); 
 
 // http://stackoverflow.com/questions/19164474/chrome-extension-get-selected-text
+$("#content").hide();
+$("#spinner").hide();
+$("#spinner").fadeIn();
 chrome.tabs.executeScript( {
   code: "window.getSelection().toString();"
 }, function(selection) {
@@ -9,74 +12,56 @@ chrome.tabs.executeScript( {
 });
 
 function loadReviews(search){
-  	var xmlhttp;
-	
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+	 search = search[0];
+	   console.log("Search term: " + search);
+	    // if(typeof splitSearch !== "undefined")
+	    if(search != "")
+	{
 
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-        	
-           if(xmlhttp.status == 200){
-           	// console.log(xmlhttp.responseText);
-           var reviews = $(xmlhttp.responseText).find(".plaincontentbox");
-           
-  // var p = document.createElement("p");
-  // p.appendChild(document.createTextNode(xmlhttp.xml));
-   // content.appendChild(p);
-   			console.log(reviews[0]);
-   			console.log(String(reviews[0]));
-   			if(typeof reviews[0] !== "undefined"){
-   				content.appendChild(reviews[0]);
-   			}else{
-   				content.append("<p>No results were found, the artist may be misspelt.</p>");
-   			}
-           	
-           	console.log("Data appended");
-          //     	var jsonText = JSON.parse(xmlhttp.responseText);
-		        // console.log(jsonText);
-         }
-           else if(xmlhttp.status == 400) {
-              
-              content.innerHTML = 'There was an error 400';
-           }
-           else {
-           	content.innerHTML = 'something else other than 200 was returned';
-           }
-        }
-    }
-    console.log(search[0]);
-    var splitSearch = search[0].split();
-    var joinSearch;
-    var sputnikQuery;
-    if(typeof splitSearch !== "undefined")
-{
-  	joinSearch = splitSearch.join("+");
-  	console.log(joinSearch);
-  	sputnikQuery = 
-		"http://www.sputnikmusic.com/search_results.php?genreid=37&search_in=Bands&search_text=" + 
-		joinSearch + "&x=0&y=0"; 
-  	
-  	console.log(sputnikQuery);
-} 
+	    var splitSearch = search.split();
+	    var joinSearch = splitSearch.join("+");
+	    // it isn't joining correctly, but the query works.
+	  
+	  	var  sputnikQuery = 
+			"http://www.sputnikmusic.com/search_results.php?genreid=37&search_in=Bands&search_text=" + 
+			joinSearch + "&x=0&y=0"; 
+			console.log(sputnikQuery);
+
+		$.ajax({
+                    url: sputnikQuery,
+                    success: function(data) {
+
+		           var reviews = $(data).find(".plaincontentbox");
 	
+		   			// console.log(reviews[0]);
+		   			// console.log(String(reviews[0]));
+		   			if(typeof reviews[0] !== "undefined"){
+		   				$("#content").append(reviews[0]);
+		   				$("#content img").each(function() {
+		   					var oldRelativeUrl = $(this).attr("src");
+		   					$(this).attr("src", "http://www.sputnikmusic.com" + oldRelativeUrl);
+						});
+						$("td form").remove();
+		   				
+		   			}else{
+		   				$("#content").append("<p>No results were found, the artist may be misspelt.</p>");
+		   			}
+
+
+                    },
+                    error:function(data){
+                    		$("#content").append("<p>Something went wrong. Please try again.</p>");
+                    }
+                });
+
+			}else{
+			// if there is no search term. Show the popup with a search box.
+			console.log("No search term selected.");
+			$("#content").append("<p>A search query was not chosen.</p>");
+			}
+
+			$("#spinner").fadeOut("fast");
+    		$("#content").fadeIn("slow");
 		
-    xmlhttp.open("GET", sputnikQuery , true);
-    xmlhttp.send();
-
- // $.ajax({
- //                    url: "http://www.sputnikmusic.com/search_results.php?genreid=37&search_in=Bands&search_text=rise+against&x=0&y=0",
- //                    success: function(data) {
- //                        $('#content').innerhtml(data);
- //                    },
- //                    error:function(data){
- //                    		$('#content').innerhtml("failed");
- //                    }
- //                });
-}
+	} 
   
